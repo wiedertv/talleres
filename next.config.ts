b8a8d4c3230d03c69e1e6f653af1a8beed6bdfee
Epadrino <epadrinoa@gmail.com>
2025-07-01 15:04:36 -0400

    ajustes visuales en el home

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Asegúrate que es https
+        hostname: 'readdy.ai',
+        port: '', // Déjalo vacío si no hay un puerto específico
+        pathname: '/api/search-image/**', // Si quieres ser más específico sobre las rutas
+      },
+      {
+        protocol: 'https', // Asegúrate que es https
+        hostname: 'randomuser.me',
+        port: '', // Déjalo vacío si no hay un puerto específico
+        pathname: '/api/portraits/**', // Si quieres ser más específico sobre las rutas
+      },
+    ],
+  },
   /* config options here */
 };
 
